e839d8e1d38b57434e4124bef26fed7bf2287d56
CamilCar <cc.carsbo@gmail.com>
2023-03-03 14:55:40 +0000

    deployment 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://i-thinkk-blog.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['i-thinkk-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
